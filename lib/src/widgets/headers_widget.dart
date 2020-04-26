fa940830eb8cd46d8ffeff526e17eff446f81f43
Max Alva <malva@peruapps.com.pe>
2020-04-25 22:21:37 -0500

    custom painter gradient

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -222,3 +222,63 @@ class _HeaderWavesPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWavesGradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavesGradientPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesGradientPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Gradient gradient = new LinearGradient(
+        colors: <Color>[
+          Color(0xff6d05f8),
+          Color(0xffc012ff),
+          Color(0xff6d05fA),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [
+          0.2,
+          0.5,
+          1.0,
+        ]);
+
+    final Rect rect = new Rect.fromCircle(
+      center: Offset(0.0, 55),
+      radius: 180,
+    );
+
+    final paint = new Paint()..shader = gradient.createShader(rect);
+
+    // paint.color = Color(0xff615aab);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 10.0;
+
+    final path = new Path();
+
+    // path.moveTo(0, size.height * 0.5);
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30,
+        size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
